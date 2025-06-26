ea436a2f7b3523ccecfbc7537abda4f56ddcfca2
Pbernal30980 <pbernal309@gmail.com>
2025-06-26 13:49:45 -0500

    resolve issues profile container

--- lib/driver_app/presentation/widgets/profile_drawer.dart
+++ lib/driver_app/presentation/widgets/profile_drawer.dart
@@ -19,86 +19,94 @@ class ProfileDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xFF4A90E2),
-        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text(
-            'Perfil',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF4A90E2),
+            borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
           ),
-          const SizedBox(height: 16),
-          CircleAvatar(
-            radius: 40,
-            backgroundColor: Colors.white,
-            backgroundImage: imageUrl != null ? NetworkImage(imageUrl!) : null,
-            child: imageUrl == null
-                ? const Icon(Icons.person, size: 40, color: Colors.grey)
-                : null,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            name,
-            style: const TextStyle(
-              fontSize: 18,
-              color: Color.fromARGB(255, 252, 252, 252),
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Text(
-                isConnected ? 'Conectado' : 'Desconectado',
+              const Text(
+                'Perfil',
                 style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.white,
+                backgroundImage: imageUrl != null
+                    ? NetworkImage(imageUrl!)
+                    : null,
+                child: imageUrl == null
+                    ? const Icon(Icons.person, size: 40, color: Colors.grey)
+                    : null,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                name,
+                style: const TextStyle(
                   fontSize: 18,
-                  color: isConnected
-                      ? const Color.fromARGB(255, 33, 216, 140)
-                      : const Color.fromARGB(255, 106, 56, 36),
+                  color: Color.fromARGB(255, 252, 252, 252),
                   fontWeight: FontWeight.w500,
                 ),
               ),
-              const SizedBox(width: 6),
-              Icon(
-                isConnected
-                    ? Icons.check_circle_outline
-                    : Icons.cancel_outlined,
-                color: isConnected
-                    ? const Color.fromARGB(255, 33, 216, 140)
-                    : Colors.red,
-                size: 20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    isConnected ? 'Conectado' : 'Desconectado',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: isConnected
+                          ? const Color.fromARGB(255, 33, 216, 140)
+                          : const Color.fromARGB(255, 106, 56, 36),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(width: 6),
+                  Icon(
+                    isConnected
+                        ? Icons.check_circle_outline
+                        : Icons.cancel_outlined,
+                    color: isConnected ? Colors.green : Colors.red,
+                    size: 20,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  children: [
+                    _buildTile(Icons.person_outline, 'Tu perfil'),
+                    _buildTile(
+                      Icons.privacy_tip_outlined,
+                      'Política de privacidad',
+                    ),
+                    _buildTile(Icons.update, 'Actualizar contraseña'),
+                    const Divider(),
+                    _buildTile(
+                      Icons.logout,
+                      'Cerrar sesión',
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-          const SizedBox(height: 24),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              children: [
-                _buildTile(Icons.person_outline, 'Tu perfil'),
-                _buildTile(
-                  Icons.privacy_tip_outlined,
-                  'Política de privacidad',
-                ),
-                _buildTile(Icons.update, 'Actualizar contraseña'),
-                const Divider(),
-                _buildTile(Icons.logout, 'Cerrar sesión', color: Colors.red),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
